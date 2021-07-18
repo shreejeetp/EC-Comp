ba4781a888d340f23c63e99c5644626185f48a48
Shreejeet Praveen <praveen.shreejeet@gmail.com>
2021-07-19 00:49:22 +0530

    created spring project

---
+++ server/src/main/java/com/shreejeetp/eccomp/EcCompApplication.java
@@ -0,0 +1,13 @@
+package com.shreejeetp.eccomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcCompApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/shreejeetp/eccomp/EcCompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreejeetp.eccomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
